c0c87d8aa847f2ea2f9182eaa83b2767245cba2d
muhammad sani <Mohammad sanisaidu@gmail.com>
2023-03-16 08:38:23 -0700

    coding is hard

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a line with puts function
+ * Return: 0
+ */
+int main(void)
+{
+printf("I am but a humble program, ");
+printf("yet I bring you a masterpiece!\n");
+printf("Behold, an array of asterisks, ");
+printf("arranged in a most exquisite form:\n");
+printf("    * * * * * * *\n");
+printf("  * * * * * * * *\n");
+printf("    * * * * * * *\n");
+printf("  * * * * * * * *\n");
+printf("    * * * * * * *\n");
+printf("  * * * * * * * *\n");
+printf("    * * * * * * *\n");
+printf("  * * * * * * * *\n");
+printf("    * * * * * * *\n");
+printf("  * * * * * * * *\n");
+printf("    * * * * * * *\n");
+printf("  * * * * * * * *\n");
+printf("    * * * * * * *\n");
+printf("Isn't it truly a work of art?\n");
+return (0);
+}
