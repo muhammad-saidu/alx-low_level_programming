71a6f554eba06ffb3b19f087fe266130ea53707a
muhammad sani <Mohammad sanisaidu@gmail.com>
2023-03-19 03:11:39 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - I sometimes suffer from insomnia
+ *
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+
+putchar('\n');
+
+return (0);
+}
